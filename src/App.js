aac483cea03218579df436c4ae714db073102ae6
Brandon Gormley <brandontgormley@gmail.com>
2021-02-03 14:51:05 -0500

    Added JS functionality
    added functionality that tracks click count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>You clicked {count} times</h2>
+      <button onClick={handleClick} className="btn">
+        Click
+      </button>
     </div>
   );
-}
+};
 
 export default App;
